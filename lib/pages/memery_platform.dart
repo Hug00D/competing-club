4c12bf342b2b1bcae478b623c7e6f13e9d67f28c
Hug00D <161666494+Hug00D@users.noreply.github.com>
2025-07-16 20:01:19 +0800

    Update memery_platform.dart

--- lib/pages/memery_platform.dart
+++ lib/pages/memery_platform.dart
@@ -1,2 +1,11 @@
-export 'memory_web.dart'
-  if (dart.library.io) 'memory_mobile.dart';
+export 'memory_web.dart' if (dart.library.io) 'memory_mobile.dart';
+
+abstract class MemoryPlatform {
+  Future<void> startRecording();
+  Future<Map<String, String?>> stopRecording();
+
+  void downloadWebAudio(String url) {} // 預設為空（只有 Web 實作）
+}
+
+/// 工廠方法會由被 export 的檔案覆蓋，不應定義預設錯誤！⬇️ 請刪除這個：
+/// MemoryPlatform getPlatformRecorder() => throw UnimplementedError();
